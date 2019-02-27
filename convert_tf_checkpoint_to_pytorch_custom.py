5d4d6afa8bc0bba06ec0940d713825868d8bf951
keep-steady <38748880+keep-steady@users.noreply.github.com>
2019-02-27 16:11:52 +0900

    Update convert_tf_checkpoint_to_pytorch_custom.py

--- convert_tf_checkpoint_to_pytorch_custom.py
+++ convert_tf_checkpoint_to_pytorch_custom.py
@@ -1,50 +1,62 @@
-## Customize convert_tf_checkpoint_to_pytorch.py
-## only need the bert folder path
+# coding=utf-8
+# Copyright 2018 The HugginFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Convert BERT checkpoint."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import re
+import re, os
 import argparse
 import tensorflow as tf
 import torch
 import numpy as np
 
-from modeling import BertConfig, BertForPreTraining
+from modeling import BertConfig, BertModel
 
-def convert_tf_checkpoint_to_pytorch(bert_model_path):
-
+def convert(bert_model_path):
+    
     tf_checkpoint_path = os.path.join(bert_model_path,'bert_model.ckpt')
     bert_config_file   = os.path.join(bert_model_path,'bert_config.json')
     pytorch_dump_path  = os.path.join(bert_model_path,'pytorch_model.bin')
-
     
-    config_path = os.path.abspath(bert_config_file)
-    tf_path = os.path.abspath(tf_checkpoint_path)
-    print("Converting TensorFlow checkpoint from {} with config at {}".format(tf_path, config_path))
+    
+    # Initialise PyTorch model
+    config = BertConfig.from_json_file(bert_config_file)
+    model = BertModel(config)
+
     # Load weights from TF model
-    init_vars = tf.train.list_variables(tf_path)
+    path = tf_checkpoint_path
+    print("Converting TensorFlow checkpoint from {}".format(path))
+
+    init_vars = tf.train.list_variables(path)
     names = []
     arrays = []
     for name, shape in init_vars:
-        print("Loading TF weight {} with shape {}".format(name, shape))
-        array = tf.train.load_variable(tf_path, name)
+        print("Loading {} with shape {}".format(name, shape))
+        array = tf.train.load_variable(path, name)
+        print("Numpy array shape {}".format(array.shape))
         names.append(name)
         arrays.append(array)
 
-    # Initialise PyTorch model
-    config = BertConfig.from_json_file(bert_config_file)
-    print("Building PyTorch model from configuration: {}".format(str(config)))
-    model = BertForPreTraining(config)
-
     for name, array in zip(names, arrays):
+        name = name[5:]  # skip "bert/"
+        print("Loading {}".format(name))
         name = name.split('/')
-        # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
-        # which are not required for using pretrained model
-        if name[-1] in ["adam_v", "adam_m"]:
-            print("Skipping {}".format("/".join(name)))
+        if name[0] in ['redictions', 'eq_relationship']:
+            print("Skipping")
             continue
         pointer = model
         for m_name in name:
@@ -52,11 +64,9 @@ def convert_tf_checkpoint_to_pytorch(bert_model_path):
                 l = re.split(r'_(\d+)', m_name)
             else:
                 l = [m_name]
+            if l[0] in set(['adam_m', 'adam_v', 'l_step']):
+                continue
             if l[0] == 'kernel':
-                pointer = getattr(pointer, 'weight')
-            elif l[0] == 'output_bias':
-                pointer = getattr(pointer, 'bias')
-            elif l[0] == 'output_weights':
                 pointer = getattr(pointer, 'weight')
             else:
                 pointer = getattr(pointer, l[0])
@@ -67,18 +77,26 @@ def convert_tf_checkpoint_to_pytorch(bert_model_path):
             pointer = getattr(pointer, 'weight')
         elif m_name == 'kernel':
             array = np.transpose(array)
-        try:
-            assert pointer.shape == array.shape
-        except AssertionError as e:
-            e.args += (pointer.shape, array.shape)
-            raise
-        print("Initialize PyTorch weight {}".format(name))
+        # try:
+        #     assert pointer.shape == array.shape
+        # except AssertionError as e:
+        #     print(pointer)
+        #     e.args += (pointer.shape, array.shape)
+        #     raise
+        # print(array)
+        print(name)
+        if name[0] == 'l_step':
+            continue
+        # if name[0] == 'kernel':
+        #     array = np.transpose(array, (1, 0))
         pointer.data = torch.from_numpy(array)
 
     # Save pytorch-model
+    torch.save(model.state_dict(), pytorch_dump_path)
+    # Save pytorch-model
     print("\nSave PyTorch model to {}".format(pytorch_dump_path))
     print('Convert TF -> Pytorch Done!!\n')
-    torch.save(model.state_dict(), pytorch_dump_path)
+    
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -89,5 +107,7 @@ if __name__ == "__main__":
                         required = True,
                         help = "Path to the pretrained bert model.")
     args = parser.parse_args()
-    convert_tf_checkpoint_to_pytorch(args.bert_model_path)
+    convert(args.bert_model_path)
+    
+    
     
